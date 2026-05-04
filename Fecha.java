ae85f210021267773c270731d461efad5afde9a0
Joaquin Ligorria <joacoligorria1@gmail.com>
2026-05-04 10:44:48 -0300

    Intercambio inicial de proyecto

---
+++ Fecha.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Fecha here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fecha
+{
+  private int day;
+  private int month;
+  private int year;
+
+ public Fecha( int day, int month, int year){
+  assert fechaValida( day, month, year): " la fecha es invalida";
+ this.day = day;
+ this.month = month;
+ this.year = year;
+}
+ private boolean  fechaValida(int day, int month, int year){
+ return true;
+}
+ public int getDay(){
+    return day;
+    }
+ public int getMonth(){
+    return month;
+    }
+ public int getYear(){
+    return year;
+    }
+}
